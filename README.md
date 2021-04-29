a313fc1046ce27c27a6aa83997ef2a3f6dfaacc9
Arthur Svensson <guimaraesarthur86@yahoo.com.br>
2021-04-29 19:57:51 -0300

    add readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# Exercícios em Java - URI
+
+Exercícios em Java no nível Iniciante do [Uri Online Judge](https://www.urionlinejudge.com.br/).
--- src/Uri_1094.java
+++ src/Uri_1094.java
@@ -2,12 +2,5 @@ public class Uri_1094 {
     public static void main(String[] args) {
 
 
-
-        /*
-         dica: para imprimir o símbolo % dentro de um printf, use %%
-           System.out.printf("Percentual de coelhos: %.2f %%%n", x);
-        */
-
-
     }
 }
